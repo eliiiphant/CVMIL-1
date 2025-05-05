25dc1684fe0b8af7156fc59fa55fde079e9b8ed7
eliiiphant <125492812+eliiiphant@users.noreply.github.com>
2025-05-05 23:01:08 +0800

    setup app

---
+++ cvmil-1/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<div class="bg-yellow-500 min-h-screen">
+    <h1 class="flex flex-row items-center justify-center font-bold text-[70px] text-white min-h-screen">banana</h1>
+</div>
+  
